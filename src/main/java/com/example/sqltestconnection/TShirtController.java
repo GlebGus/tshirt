d5ef6bab16fde15af4ab5b4ac10054ac05d7803c
GlebGus <gusinec.lion.com@tut.by>
2023-08-02 01:04:06 +0300

    add cart

--- src/main/java/com/example/sqltestconnection/TShirtController.java
+++ src/main/java/com/example/sqltestconnection/TShirtController.java
@@ -1,7 +1,10 @@
 package com.example.sqltestconnection;
 
+import jakarta.servlet.http.HttpSession;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,8 +21,7 @@ public class TShirtController {
 
     @Autowired
     private TShirtsService tShirtsService;
-    @Autowired
-private ImageRepository imageRepository;
+
     @GetMapping("/tshirts")
     public String getAllTShirts(Model model) {
         List<TShirt> tShirts = tShirtRepository.findAll();
@@ -32,21 +34,40 @@ private ImageRepository imageRepository;
         model.addAttribute("tShirt", new TShirt());
         return "create-tshirt";
     }
-@PostMapping("/tshirts/delete/{id}")
-public String deleteTShirt(@PathVariable Long id){
-        tShirtRepository.deleteById(id);
-return "redirect:/tshirts";
-}
-
 
     @PostMapping("/tshirts/create")
     public String createTShirt(@ModelAttribute("tShirt") TShirt tShirt,
                                @RequestParam("file1") MultipartFile file1,
-                               @RequestParam("file2") MultipartFile file2)  throws IOException{
-        tShirtsService.saveTShirt(tShirt,file1,file2);
+                               @RequestParam("file2") MultipartFile file2) throws IOException {
+        tShirtsService.saveTShirt(tShirt, file1, file2);
         return "redirect:/tshirts";
     }
 
+    @PostMapping("/tshirts/delete/{id}")
+    public String deleteTShirt(@PathVariable Long id) {
+        tShirtRepository.deleteById(id);
+        return "redirect:/tshirts";
+    }
+
+    @RequestMapping(value = "/cart/add", method = {RequestMethod.GET, RequestMethod.POST})
+    public String addToCart(@RequestParam("tShirtid") Long id, HttpSession session) {
+        List<TShirt> cartItems = (List<TShirt>) session.getAttribute("cartItems");
+if (cartItems == null){
+    cartItems = new ArrayList<>();
+}
+
+TShirt tShirt = tShirtRepository.findById(id).orElse(null);
+        Hibernate.initialize(tShirt.getImages());
+cartItems.add(tShirt);
+session.setAttribute("cartItems", cartItems);
+return "redirect:/tshirts";
+    }
+@GetMapping("/cart")
+public String showCart(Model model, HttpSession session){
+        List<TShirt> cartItems = (List<TShirt>) session.getAttribute("cartItems");
+        model.addAttribute("cartItems", cartItems);
+        return "cart";
+}
 
     @GetMapping("/tshirts/sortByPrice")
     public String sortByPrice(Model model, @RequestParam("order") String order) {
@@ -59,6 +80,7 @@ return "redirect:/tshirts";
         model.addAttribute("tShirts", tShirts);
         return "tshirts";
     }
+
     @GetMapping("/tshirts/sortByAvailable")
     public String sortByAvailable(Model model, @RequestParam("available") String available) {
         List<TShirt> tShirts;
@@ -70,6 +92,7 @@ return "redirect:/tshirts";
         model.addAttribute("tShirts", tShirts);
         return "tshirts";
     }
+
     @GetMapping("/tshirts/sortBySize")
     public String sortBySize(@RequestParam("size") String size, Model model) {
         List<TShirt> tShirts;
--- src/main/java/com/example/sqltestconnection/TShirtRepository.java
+++ src/main/java/com/example/sqltestconnection/TShirtRepository.java
@@ -1,7 +1,9 @@
 package com.example.sqltestconnection;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
